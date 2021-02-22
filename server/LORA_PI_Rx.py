6872f5664e63b919f0bf3a573f7550a5af4a3196
Han Diep <diephan26@gmail.com>
2021-02-22 17:01:40 -0600

    added logging feature

--- server/LORA_PI_Rx.py
+++ server/LORA_PI_Rx.py
@@ -1,8 +1,12 @@
 from time import sleep
 from SX127x.LoRa import *
 from SX127x.board_config import BOARD
+import logging
 
 BOARD.setup()
+
+#set up logging
+logger = logging.Logger
 
 class LoRaRcvCont(LoRa):
     def __init__(self, verbose=False):
@@ -24,26 +28,39 @@ class LoRaRcvCont(LoRa):
         print("\nReceived: ")
         self.clear_irq_flags(RxDone=1)
         payload = self.read_payload(nocheck=True)
-        print(bytes(payload).decode("utf-8",'ignore'))
+        decoded_payload = bytes(payload).decode("utf-8",'ignore') 
+        print(decoded_payload)
+        logging.info(decoded_payload)
         self.set_mode(MODE.SLEEP)
         self.reset_ptr_rx()
         self.set_mode(MODE.RXCONT) 
 
-lora = LoRaRcvCont(verbose=False)
-lora.set_mode(MODE.STDBY)
+if __name__ == '__main__':
+    logging.basicConfig(
+        filename='FieldTest.log',
+        filemode='w+',
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=logging.DEBUG)
 
-#  Medium Range  Defaults after init are 434.0MHz, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on 13 dBm
+    logging.info('-------------- PROGRAM START --------------')
 
-lora.set_pa_config(pa_select=1)
+    lora = LoRaRcvCont(verbose=False)
+    lora.set_mode(MODE.STDBY)
 
-try:
-    lora.start()
-except KeyboardInterrupt:
-    sys.stdout.flush()
-    print("")
-    sys.stderr.write("KeyboardInterrupt\n")
-finally:
-    sys.stdout.flush()
-    print("")
-    lora.set_mode(MODE.SLEEP)
-    BOARD.teardown()
+    #  Medium Range  Defaults after init are 434.0MHz, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on 13 dBm
+
+    lora.set_pa_config(pa_select=1)
+
+    try:
+        lora.start()
+    except KeyboardInterrupt:
+        sys.stdout.flush()
+        print("")
+        sys.stderr.write("KeyboardInterrupt\n")
+    finally:
+        sys.stdout.flush()
+        print("")
+        lora.set_mode(MODE.SLEEP)
+        logging.info('--------------- PROGRAM END ---------------')
+        BOARD.teardown()
